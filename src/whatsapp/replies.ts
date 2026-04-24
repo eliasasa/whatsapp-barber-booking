498ac13100a316703ddea4122260ac83e72716f7
eliasasa <eliasnetoneto28@gmail.com>
2026-04-24 00:54:40 -0400

    make bot name configurable

--- src/whatsapp/replies.ts
+++ src/whatsapp/replies.ts
@@ -1,6 +1,8 @@
+import { BOT_NAME } from "../global/botConfig";
+
 export const replies = {
     greeting:
-        "Oi! 👋 Sou o assistente do Hadriel.\n\nVocê pode:\n - Agendar um horário\n - Ver horários disponíveis\n\nÉ só me dizer o que você quer 😊",
+        `Oi! 👋 Sou o assistente do ${BOT_NAME}.\n\nVocê pode:\n - Agendar um horário\n - Ver horários disponíveis\n\nÉ só me dizer o que você quer 😊`,
 
     unknown:
         "Não entendi muito bem 😅\nVocê pode dizer:\n- Quero agendar\n- Ver horários\n- Cancelar",
