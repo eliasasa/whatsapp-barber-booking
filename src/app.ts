be58ad2e90b3d184c6d623c51375750bdc9ab37b
eliasasa <eliasnetoneto28@gmail.com>
2026-07-04 18:29:25 -0400

    fix(cors): enable credentials and dynamic origin

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,10 @@ import cors from "cors";
 
 export const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
